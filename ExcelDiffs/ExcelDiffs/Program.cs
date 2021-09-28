4fb5d7ec43a2c0c7e16b7778ca04ce325aed91f2
RalucaIonita <raluca.ionita04@gmail.com>
2021-09-28 15:21:07 +0300

    [Initial] Project structure

---
+++ ExcelDiffs/ExcelDiffs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExcelDiffs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
